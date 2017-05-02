517e2c28c2a8c5c51b51d4ca0580f5ad2afc26d0
rendaniluk <lukhwa11582924@gmail.com>
2017-05-02 15:08:52 +0200

    Update FutureValue.tests.js

--- test/FutureValue.tests.js
+++ test/FutureValue.tests.js
@@ -1,7 +1,7 @@
 describe('The FutureValue function', function(){
 
     it('should return the value of investment in future', function(){
-        assert.equal(2831.53, FutureValue(1000,(10/100),5,1,200));
+        assert.equal(1610.51, FutureValue(1000,(10/100),5,1));
     });
     it('should return a NaN if parameters are letter string', function(){
 
