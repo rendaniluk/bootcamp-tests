b4a2b581176fea5f312d591c6a25424cb9c3575f
rendaniluk <lukhwa11582924@gmail.com>
2017-05-02 15:07:59 +0200

    Update FutureValue.js

--- FutureValue.js
+++ FutureValue.js
@@ -1,8 +1,8 @@
 //calculator
 function FutureValue(initialDepositValue, annualInterestValue, numberOfyearsValue, interestTimesValue, monthlyContributionValue) {
-  A = initialDepositValue * Math.pow(1 + (annualInterestValue/interestTimesValue), (numberOfyearsValue*interestTimesValue)) + monthlyContributionValue*12*((Math.pow(1 + (annualInterestValue/interestTimesValue), (numberOfyearsValue*interestTimesValue)) - 1)/annualInterestValue);
+  A = initialDepositValue * Math.pow(1 + (annualInterestValue/interestTimesValue), (numberOfyearsValue*interestTimesValue));
   return A.toFixed(2);
 };
 //function Call
-myValue = FutureValue("r",("s"/'k'),"v","m");
+myValue = FutureValue("r",("s"/'k'),"v");
 console.log(myValue);
